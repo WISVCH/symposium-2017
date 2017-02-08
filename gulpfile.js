b495e86d327f1ae4c621fea9f26f8dca73481d76
Tim van der Lippe <tvanderlippe@gmail.com>
2017-02-08 13:03:16 +0100

    Update build with new polymer-build 0.7.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,45 @@
-/**
- * @license
- * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
-
+/*
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at
+http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at
+http://polymer.github.io/PATENTS.txt
+*/
 'use strict';
 
 const path = require('path');
 const gulp = require('gulp');
 const gulpif = require('gulp-if');
-const uglify = require('gulp-uglify');
+const babel = require('gulp-babel')
 const cssSlam = require('css-slam').gulp;
 const htmlMinifier = require('gulp-html-minifier');
+const jshint = require('gulp-jshint');
 
 // Got problems? Try logging 'em
 // const logging = require('plylog');
 // logging.setVerbose();
 
-// !!! IMPORTANT !!! //
-// Keep the global.config above any of the gulp-tasks that depend on it
-global.config = {
-  polymerJsonPath: path.join(process.cwd(), 'polymer.json'),
-  build: {
-    rootDirectory: 'build',
-    bundledDirectory: 'bundled',
-    unbundledDirectory: 'unbundled',
-    // Accepts either 'bundled', 'unbundled', or 'both'
-    // A bundled version will be vulcanized and sharded. An unbundled version
-    // will not have its files combined (this is for projects using HTTP/2
-    // server push). Using the 'both' option will create two output projects,
-    // one for bundled and one for unbundled
-    bundleType: 'both'
-  },
-  // Path to your service worker, relative to the build root directory
-  serviceWorkerPath: 'service-worker.js',
-  // Service Worker precache options based on
-  // https://github.com/GoogleChrome/sw-precache#options-parameter
-  swPrecacheConfig: require('./sw-precache-config.json')
-};
-
 // Add your own custom gulp tasks to the gulp-tasks directory
 // A few sample tasks are provided for you
 // A task should return either a WriteableStream or a Promise
 const clean = require('./gulp-tasks/clean.js');
-const images = require('./gulp-tasks/images.js');
 const project = require('./gulp-tasks/project.js');
 
 function minify() {
   return htmlMinifier({
-    collapseWhitespace: true,
-    removeComments: true,
-    removeAttributeQuotes: true,
-    removeRedundantAttributes: true,
-    useShortDoctype: true,
-    removeEmptyAttributes: true,
-    removeScriptTypeAttributes: true,
-    removeStyleLinkTypeAttributes: true,
-    removeOptionalTags: true
+    collapseWhitespace : true,
+    removeComments : true,
+    removeAttributeQuotes : true,
+    removeRedundantAttributes : true,
+    useShortDoctype : true,
+    removeEmptyAttributes : true,
+    removeScriptTypeAttributes : true,
+    removeStyleLinkTypeAttributes : true,
+    removeOptionalTags : true
   });
 }
 
@@ -73,13 +52,12 @@ function minify() {
 // which filters all images and runs them through imagemin
 function source() {
   return project.splitSource()
-    // Add your own build tasks here!
-    .pipe(gulpif('**/*.{png,gif,jpg,svg}', images.minify()))
-    .pipe(gulpif(/\.js$/, uglify()))
-    .pipe(gulpif(/\.css$/, cssSlam()))
-    .pipe(gulpif(/\.html$/, cssSlam()))
-    .pipe(gulpif(/\.html$/, minify()))
-    .pipe(project.rejoin());
+      // Add your own build tasks here!
+      .pipe(gulpif(/\.js$/, babel({presets: ['babili']})))
+      .pipe(gulpif(/\.css$/, cssSlam()))
+      .pipe(gulpif(/\.html$/, cssSlam()))
+      .pipe(gulpif(/\.html$/, minify()))
+      .pipe(project.rejoin());
 }
 
 // The dependencies task will split all of your bower_components files into one
@@ -88,18 +66,43 @@ function source() {
 // case you need it :)
 function dependencies() {
   return project.splitDependencies()
-    .pipe(gulpif(/\.js$/, uglify()))
-    .pipe(gulpif(/\.css$/, cssSlam()))
-    .pipe(gulpif(/\.html$/, cssSlam()))
-    .pipe(gulpif(/\.html$/, minify()))
-    .pipe(project.rejoin());
+      .pipe(gulpif(/\.js$/, babel({presets: ['babili']})))
+      .pipe(gulpif(/\.css$/, cssSlam()))
+      .pipe(gulpif(/\.html$/, cssSlam()))
+      .pipe(gulpif(/\.html$/, minify()))
+      .pipe(project.rejoin());
+}
+
+const scaffoldIndexes = require('./gulp-tasks/scaffold-indexes.js');
+
+gulp.task('scaffold-indexes', gulp.series([
+  clean([ 'index-build' ]),
+  scaffoldIndexes.scaffold
+]));
+
+const optimizeImages = require('./gulp-tasks/optimize-images.js');
+
+// Optimize images with ImageOptim
+// Run with `yarn run build optimize-images`
+gulp.task('optimize-images', optimizeImages.optimizeImages);
+gulp.task('ensure-images-optimized', optimizeImages.ensureOptimizeImages);
+
+function linter() {
+  return gulp.src([ 'scripts/**/*.js',
+                    'src/**/*.html' ])
+      .pipe(jshint.extract()) // Extract JS from .html files
+      .pipe(jshint())
+      .pipe(jshint.reporter('jshint-stylish'));
 }
 
 // Clean the build directory, split all source and dependency files into streams
 // and process them, and output bundled and unbundled versions of the project
 // with their own service workers
 gulp.task('default', gulp.series([
-  clean([global.config.build.rootDirectory]),
+  clean([ 'build', 'index-build' ]),
+  scaffoldIndexes.scaffold,
   project.merge(source, dependencies),
+  scaffoldIndexes.mergeIntoBuild,
+  clean([ path.resolve('build', 'index-build'), 'index-build' ]),
   project.serviceWorker
 ]));
