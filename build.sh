7bd12dab077612829b78c9d6a50a24edf9a0ed72
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2017-03-11 23:02:39 +0100

    Fix flickering of program with nginx.conf (#82)

--- build.sh
+++ build.sh
@@ -5,23 +5,24 @@ set -e
 apk --no-cache add nodejs git curl
 apk --update add tar
 curl -o- -L https://yarnpkg.com/install.sh | sh
-$HOME/.yarn/bin/yarn global add bower
+export PATH="$HOME/.yarn/bin:$PATH"
+yarn global add bower
 
 # Fetch dependencies
-$HOME/.yarn/bin/yarn
+yarn
 bower --allow-root install
 
 # compress images and build
-$HOME/.yarn/bin/yarn run build optimize-images
-$HOME/.yarn/bin/yarn run build
+yarn run build optimize-images
+yarn run build
 mkdir -p /srv/www/2017
 cp -r ./build/. /srv/www/2017/
 
 # Uninstall installed build dependencies and remove cache
-$HOME/.yarn/bin/yarn cache clean
+yarn cache clean
 bower --allow-root cache clean
 npm cache clean
-$HOME/.yarn/bin/yarn global remove bower
+yarn global remove bower
 apk del nodejs git curl tar
 
 #Delete folder used for build
